0be7e28be834a2f29acdb488986784113795ac1e
Matteias Collet <matteias.collet@bluewin.ch>
2019-11-28 08:14:12 +0100

    add pre-poll

--- app/Configuration/Questions.php
+++ app/Configuration/Questions.php
@@ -4,9 +4,23 @@ namespace App\Configuration;
 
 class Questions
 {
+
+  public const QUESTION_LIST = [
+    self::AFF_CATEGORIES,
+    self::ALLOW_MULTIPLE
+  ];
+
+  public const POLL_DESCRIPTION = '<b>Voting system</b>: To be able to vote you need a <a href="https://speedrun.com"
+        target="_blank" rel="noreferrer">speedrun.com</a> account. Votes from people with runs on the Super Mario Sunshine
+        main or category extension leaderboards are automatically verified. All other votes will go through a process of
+        manual verification.';
+
+  /*
+   *
+   * MAIN POLL
+
   public const QUESTION_LIST = [
     self::HIDE_TIMINGS,
-    self::ALLOW_MULTIPLE,
     self::TIMING_METHOD_A,
     self::TIMING_METHOD_B,
     self::TIMING_METHOD_C,
@@ -15,11 +29,65 @@ class Questions
     self::COMMENT
   ];
 
-  public const POLL_DESCRIPTION = '<b>Voting system</b>: Everyone will be voting on whether or not they agree that a timing method should be
-      allowed on the leaderboard, as well as specificites contained in that, should one or more of the timing
-      methods reach over 50%, the method with the highest approval rating will be implemented alongside file
-      select on the leaderboard. In the event that timing method C or B wins, and other method also receives over
-      50%, adding both of them will be considered.';
+  public const POLL_DESCRIPTION = '<b>Voting system</b>: Everyone will be voting on whether or not they agree
+      that a timing method should be allowed on the leaderboard, as well as specificites contained in that, 
+      should one or more of the timing methods reach over 50%, the method with the highest approval rating will 
+      be implemented alongside file select on the leaderboard. In the event that timing method C or B wins, and 
+      other method also receives over 50%, adding both of them will be considered.';
+  */
+
+  private const AFF_CATEGORIES = [
+    'id' => 'v_aff_categories',
+    'title' => 'Affected Categories',
+    'type' => 'select',
+    'required' => true,
+    'options' => [
+      [
+        'value' => 'Indifferent',
+        'label' => 'Indifferent',
+      ],
+      [
+        'value' => 'All main categories',
+        'label' => 'All main categories',
+      ],
+      [
+        'value' => 'Any% Only',
+        'label' => 'Any% Only'
+      ]
+    ],
+    'default' => 'Indifferent',
+    'description' => 'What categories should be affected in case of a new timing method being implemented?',
+    'validation_error' => 'The option chosen for "Affected Categories" is invalid'
+  ];
+
+  private const ALLOW_MULTIPLE = [
+    'id' => 'v_allow_multiple',
+    'title' => 'Allow multiple timing methods',
+    'type' => 'select',
+    'required' => true,
+    'options' => [
+      [
+        'value' => 'Indifferent',
+        'label' => 'Indifferent',
+      ],
+      [
+        'value' => 'Any that gain 50% or more',
+        'label' => 'Any that gain 50% or more',
+      ],
+      [
+        'value' => 'Only the most voted for',
+        'label' => 'Only the most voted for'
+      ],
+      [
+        'value' => 'Peach file if it gets 50% or more and the most voted of the others',
+        'label' => 'Peach file if it gets 50% or more and the most voted of the others'
+      ]
+    ],
+    'default' => 'Indifferent',
+    'description' => 'In the event that more than one timing method gains over a 50 percent majority, should more than one be allowed?',
+    'validation_error' => 'The value selected for "Allow multiple timing methods" is invalid'
+  ];
+
 
   private const HIDE_TIMINGS = [
     'id' => 'v_hide_timings',
@@ -45,34 +113,6 @@ class Questions
     'validation_error' => 'The option chosen for "Hide Timings" is invalid'
   ];
 
-  private const ALLOW_MULTIPLE = [
-    'id' => 'v_allow_multiple',
-    'title' => 'Allow multiple timing methods',
-    'type' => 'select',
-    'required' => true,
-    'options' => [
-      [
-        'value' => 'Indifferent',
-        'label' => 'Indifferent',
-      ],
-      [
-        'value' => 'Yes',
-        'label' => 'Yes',
-      ],
-      [
-        'value' => 'No',
-        'label' => 'No'
-      ],
-      [
-        'value' => 'Only B and C',
-        'label' => 'Only B and C'
-      ]
-    ],
-    'default' => 'Indifferent',
-    'description' => 'In the event that more than one timing method gains over a 50 percent majority, should more than one be allowed?',
-    'validation_error' => 'The value selected for "Allow multiple timing methods" is invalid'
-  ];
-
   private const TIMING_METHOD_A = [
     'id' => 'v_option_a',
     'title' => 'Timing Method A',
@@ -93,7 +133,9 @@ class Questions
       ]
     ],
     'default' => 'Indifferent',
-    'description' => 'Timing starts on closing Peach’s textbox. The file used is a premade file saved immediately after talking to peach on the airstrip. The first cutscene is skipped. Around a minute shorter than file select.<br><br>Sample: <a href="https://www.youtube.com/watch?v=-lML1_C1mfg" target="_blank" rel="noreferrer">Youtube</a>',
+    'description' => 'Timing starts on closing Peach’s textbox. The file used is a premade file saved immediately 
+        after talking to peach on the airstrip. The first cutscene is skipped. Around a minute shorter than file 
+        select.<br><br>Sample: <a href="https://www.youtube.com/watch?v=-lML1_C1mfg" target="_blank" rel="noreferrer">Youtube</a>',
     'validation_error' => 'The option chosen for "Timing Method A" is invalid'
   ];
 
@@ -117,7 +159,10 @@ class Questions
       ]
     ],
     'default' => 'Indifferent',
-    'description' => 'The file used is a premade file saved after having watched the Fludd cutscene on airstrip. When loading this file, the intro cutscene and the fludd cutscene may be skipped. Around 2 minutes shorter than file select.<br><br>Sample: <a href="https://www.youtube.com/watch?v=l94wg1wjuZs" target="_blank" rel="noreferrer">Youtube</a>',
+    'description' => 'The file used is a premade file saved after having watched the Fludd cutscene on airstrip. 
+          When loading this file, the intro cutscene and the fludd cutscene may be skipped. Around 2 minutes 
+          shorter than file select.<br><br>Sample: <a href="https://www.youtube.com/watch?v=l94wg1wjuZs" 
+          target="_blank" rel="noreferrer">Youtube</a>',
     'validation_error' => 'The option chosen for "Timing Method B" is invalid'
   ];
 
@@ -141,7 +186,10 @@ class Questions
       ]
     ],
     'default' => 'Indifferent',
-    'description' => 'Two premade files are used. One is saved after having watched the Fludd cutscene on airstrip. The second is saved after being loaded into delfino plaza. The player would load the first file, complete airstrip, reset, load the 2nd file and continue the run. Around 3 minutes faster than file select.<br><br>Sample: <a href="https://youtu.be/h6xMV6M8MQs" target="_blank" rel="noreferrer">Youtube</a>',
+    'description' => 'Two premade files are used. One is saved after having watched the Fludd cutscene on airstrip. 
+          The second is saved after being loaded into delfino plaza. The player would load the first file, 
+          complete airstrip, reset, load the 2nd file and continue the run. Around 3 minutes faster than file 
+          select.<br><br>Sample: <a href="https://youtu.be/h6xMV6M8MQs" target="_blank" rel="noreferrer">Youtube</a>',
     'validation_error' => 'The option chosen for "Timing Method C" is invalid'
   ];
 
@@ -165,7 +213,8 @@ class Questions
       ]
     ],
     'default' => 'Indifferent',
-    'description' => 'A save file that has been pre-modified and loaded onto your memory card, having set the intro cutscene, fludd cutscene, and courtroom cutscene to watched, allowing them to be skipped. Around 3 minutes faster than file select.',
+    'description' => 'A save file that has been pre-modified and loaded onto your memory card, having set the intro cutscene, 
+    fludd cutscene, and courtroom cutscene to watched, allowing them to be skipped. Around 3 minutes faster than file select.',
     'validation_error' => 'The option chosen for "Timing Method D" is invalid'
   ];
 
@@ -189,7 +238,9 @@ class Questions
       ]
     ],
     'default' => 'Indifferent',
-    'description' => 'A save file that has been pre-modified and loaded onto your memory card, having set all cutscene flags to watched, allowing every cutscene to be skipped. Mashing through a cutscene adds its length to the final time to preserve in-game skips. Around 7 minutes faster than file select.',
+    'description' => 'A save file that has been pre-modified and loaded onto your memory card, having set all cutscene flags to watched, 
+        allowing every cutscene to be skipped. Mashing through a cutscene adds its length to the final time to preserve in-game skips.
+        Around 7 minutes faster than file select.',
     'validation_error' => 'The option chosen for "Timing Method E" is invalid'
   ];
 
