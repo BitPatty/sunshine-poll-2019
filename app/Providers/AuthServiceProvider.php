2be91e209cecd4d0b28d25a8216fd4dca76b209f
Matteias Collet <matteias.collet@bluewin.ch>
2020-01-25 13:30:42 +0100

    lc auth roles

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,14 +28,16 @@ class AuthServiceProvider extends ServiceProvider
 
         Gate::define('read-votes', function (User $user) {
             if (!isset($user)) return false;
+            if (!isset($user->src_name) || empty($user->src_name)) return false;
             $audits = array_unique(explode(',', env('ROLE_AUDIT') . ',' . env('ROLE_MOD') . ',' . env('ROLE_ADMIN')));
-            return (in_array($user->src_name, $audits));
+            return (in_array(strtolower($user->src_name), $audits));
         });
 
         Gate::define('update-votes', function (User $user) {
             if (!isset($user)) return false;
+            if (!isset($user->src_name) || empty($user->src_name)) return false;
             $audits = array_unique(explode(',', env('ROLE_MOD')));
-            return (in_array($user->src_name, $audits));
+            return (in_array(strtolower($user->src_name), $audits));
         });
     }
 }
