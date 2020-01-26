39903ec2bcd8e6b71072acd110e3d33c61a07f95
Matteias Collet <matteias.collet@bluewin.ch>
2020-01-26 14:20:01 +0100

    improve redirect after logon

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\VerificationState;
-use App\Models\Vote;
-use App\Models\VoteHistory;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
@@ -56,7 +52,7 @@ class AuthController extends Controller
             return Redirect::back()->withErrors(['error' => 'Missing authorization']);
         }
 
-        return redirect('/verification');
+        return redirect()->intended('/verification');
     }
 
     public function logout()
